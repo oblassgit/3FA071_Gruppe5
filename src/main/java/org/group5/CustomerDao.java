9cef838cac0a93b2b6cc021d0fc2c627fc056a9d
Oliver Blass <oliverblass@web.de>
2024-10-11 08:57:20 +0200

    implemented some of the Methods in CustomerDao

---
+++ src/main/java/org/group5/CustomerDao.java
@@ -0,0 +1,138 @@
+package org.group5;
+
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.UUID;
+
+public class CustomerDao implements ICustomer {
+
+    private UUID id;
+
+    private Connection connection = Util.getConnection("DbData");
+
+
+    public CustomerDao(UUID id, String firstName, String lastName, LocalDate birthDate, Gender gender) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("insert into Customer (id, first_name, last_name, birth_date, gender) VALUES (?, ?, ?, ?, ?)");
+        statement.setString(1, id.toString());
+        statement.setString(2, firstName);
+        statement.setString(3, lastName);
+        statement.setDate(4, Date.valueOf(birthDate));
+        statement.setString(5, gender.toString());
+        statement.execute();
+
+        this.id = id;
+    }
+
+    public void deleteCustomer(UUID id) throws SQLException {
+        PreparedStatement statement = connection.prepareStatement("delete from Customer as customer where customer.id = ?");
+        statement.setString(1, id.toString());
+
+        statement.execute();
+    }
+
+    @Override
+    public void setFirstName(String firstName) {
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement("update Customer set first_name = ? where id = ?");
+            statement.setString(1, firstName);
+            statement.setString(2, id.toString());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    public void setLastName(String lastName) {
+        PreparedStatement statement;
+        try {
+            statement = connection.prepareStatement("update Customer set last_name = ? where id = ?");
+            statement.setString(1, lastName);
+            statement.setString(2, id.toString());
+            statement.execute();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    @Override
+    public void setBirthDate(LocalDate birthDate) {
+
+    }
+
+    @Override
+    public void setGender(Gender gender) {
+
+    }
+
+    @Override
+    public String getFirstName() {
+        PreparedStatement statement;
+        String name = "";
+
+        try {
+            statement = connection.prepareStatement("select first_name from Customer where id = ?");
+            statement.setString(1, String.valueOf(id));
+            statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            if (resultSet.next()) {
+                name = resultSet.getString("first_name");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return name;
+
+    }
+
+    @Override
+    public String getLastName() {
+        PreparedStatement statement;
+        String name = "";
+
+        try {
+            statement = connection.prepareStatement("select last_name from Customer where id = ?");
+            statement.setString(1, String.valueOf(id));
+            statement.execute();
+            ResultSet resultSet = statement.getResultSet();
+            if (resultSet.next()) {
+                name = resultSet.getString("last_name");
+            }
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
+        return name;
+    }
+
+    @Override
+    public LocalDate getBirthDate() {
+        return null;
+    }
+
+    @Override
+    public Gender getGender() {
+        return null;
+    }
+
+    @Override
+    public UUID getId() {
+        return id;
+    }
+
+    @Override
+    public void setId(UUID id) {
+        this.id = id;
+    }
+}
