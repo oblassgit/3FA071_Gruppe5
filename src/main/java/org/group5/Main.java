d4a761f7ae026dedb7ae84b39f554c56493b16d3
Oliver Blass <oliverblass@web.de>
2024-10-11 10:39:03 +0200

    fixed deleteCustomer Method

--- src/main/java/org/group5/Main.java
+++ src/main/java/org/group5/Main.java
@@ -1,7 +1,19 @@
 package org.group5;
 
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.UUID;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws SQLException {
+
+        UUID uuid = UUID.randomUUID();
+
+        CustomerDao customerDao = new CustomerDao(uuid, "Oliver", "Blass", LocalDate.now(), Gender.M);
+        System.out.println(customerDao.getFirstName() + " " + customerDao.getLastName());
+        System.out.println(customerDao.getGender());
+
+        customerDao.deleteCustomer();
+
     }
 }
