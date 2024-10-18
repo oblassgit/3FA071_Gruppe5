97edbdec4a18549f6ddbd5126d9d14cf9c75c14e
KilisAccount <vogel.kilian@online.de>
2024-10-18 12:03:48 +0200

    Added Constructor for Util

--- src/main/java/dev/hv/Util.java
+++ src/main/java/dev/hv/Util.java
@@ -1,70 +1,40 @@
 package dev.hv;
+
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Properties;
 
 public class Util {
-    String dburl = "";
-    String dbuser = "";
-    String dbpw = "";
+    private String dburl;
+    private String dbuser;
+    private String dbpw;
+    private Properties properties = new Properties();
 
-    public Properties getDbProperties() {
-        String propertiesFilename = "src/main/resources/DbData.properties";
-        FileInputStream fileInputStream = null;
+    public Util() {
+        try {
 
-        try {
-            fileInputStream = new FileInputStream(propertiesFilename);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        }
-        Properties properties = new Properties();
-        try {
+            FileInputStream fileInputStream = new FileInputStream("src/main/resources/DbData.properties");
             properties.load(fileInputStream);
+
+            dburl = properties.getProperty(System.getProperty("user.name") + ".db.url");
+            dbuser = properties.getProperty(System.getProperty("user.name") + ".db.user");
+            dbpw = properties.getProperty(System.getProperty("user.name") + ".db.pw");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        final String dburl = properties.getProperty(System.getProperty("user.name") + ".db.url");
-        final String dbuser = properties.getProperty(System.getProperty("user.name") + ".db.user");
-        final String dbpw = properties.getProperty(System.getProperty("user.name") + ".db.pw");
-
-        setDbUrl(dburl);
-        setDbUser(dbuser);
-        setDbPw(dbpw);
-        
-        return properties;
-    }
-
-    public void setDbUrl(String dburl) {
-        this.dburl = dburl;
     }
 
     public String getDbUrl() {
         return dburl;
     }
 
-    public void setDbUser(String dbuser) {
-        this.dbuser = dbuser;
-    }
-
     public String getDbUser() {
         return dbuser;
-    }
-
-    public void setDbPw(String dbpw) {
-        this.dbpw = dbpw;
     }
 
     public String getDbPw() {
         return dbpw;
     }
-
-
-        
-    }
-    
-
-
-    
-
+}
