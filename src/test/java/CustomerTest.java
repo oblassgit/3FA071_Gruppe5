83956c2832ed46b0e5cd802149e274f29120409b
KilisAccount <vogel.kilian@online.de>
2024-10-18 09:39:18 +0200

    implemente toString tests for customer and reading

--- src/test/java/CustomerTest.java
+++ src/test/java/CustomerTest.java
@@ -18,13 +18,15 @@ public class CustomerTest {
 
     static DatabaseCon databaseCon = new DatabaseCon();
     static Customer customer;
+    static UUID customerId;
 
     @BeforeEach
     public void before() throws IOException {
         Properties properties = new Properties();
         properties.load(new FileReader("src/main/resources/DbData.properties"));
         databaseCon.openConnections(properties);
-        customer = new Customer(UUID.randomUUID(), "Schwanzus", "Longus", LocalDate.of(2005, 6, 9), Gender.M);
+        customerId = UUID.randomUUID();
+        customer = new Customer(customerId, "Schwanzus", "Longus", LocalDate.of(2005, 6, 9), Gender.M);
         databaseCon.removeAllTables();
         databaseCon.createAllTables();
         databaseCon.truncateAllTables();
@@ -44,6 +46,12 @@ public class CustomerTest {
         assertEquals(TestDate, customer.getBirthDate());
         assertEquals(Gender.D, customer.getGender());
 
+    }
+
+    @Test
+    public void testToString() {
+        assertEquals(customer.toString(), "Customer{firstName='Schwanzus', lastName='Longus', birthDate="
+                + LocalDate.of(2005, 6, 9) + ", gender=" + Gender.M + ", id=" + customerId + '}');
     }
 
     @AfterAll
