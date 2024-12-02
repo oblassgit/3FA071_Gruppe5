e6262691f3f21a361bd5148d59c82f3b6b325d7b
David Zorn <dave_king@outlook.de>
2024-12-02 08:59:47 +0100

    GET customers

--- src/main/java/dev/hv/rest/resource/CustomerResource.java
+++ src/main/java/dev/hv/rest/resource/CustomerResource.java
@@ -3,6 +3,7 @@ package dev.hv.rest.resource;
 import dev.hv.Util;
 import dev.hv.db.CustomerDao;
 import dev.hv.db.DatabaseCon;
+import dev.hv.enums.Gender;
 import dev.hv.model.Customer;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
@@ -12,6 +13,8 @@ import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.util.List;
 import java.util.UUID;
 
 @Path("customers")
@@ -20,6 +23,33 @@ public class CustomerResource {
     private Util util = new Util();
     private DatabaseCon databaseCon = new DatabaseCon();
 
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getCustomer() {
+        CustomerList customerList;
+
+
+        try {
+            databaseCon.openConnections(util.getProperties());
+            databaseCon.createAllTables();
+
+            CustomerDao customerDao = new CustomerDao(databaseCon.getConnection());
+            customerList = new CustomerList(customerDao.getAllCustomers());
+
+        } catch (SQLException e) {
+            return Response.serverError().build();
+        }
+
+        if(customerList.isEmpty()) {
+            System.out.println("Liste ist leer");
+        } else {
+            System.out.println("......");
+            System.out.println(customerList.toString());
+        }
+        
+        return Response.ok(customerList).build();
+    }
 
     @Path("/{uuid}")
     @GET
@@ -42,7 +72,10 @@ public class CustomerResource {
         //Customer customer = new Customer(UUID.randomUUID(), "Hans", "Wurst", LocalDate.now(), Gender.M);
         return Response.ok(customer).build();
     }
+
+
 }
+
 
 //
 
