bb97521a250812ba56a0c28168068d397d4211e7
Oliver Blass <oliverblass@web.de>
2024-12-06 10:47:51 +0100

    added json schemas and implemented response schema check

--- src/test/java/rest/CustomerResourceTest.java
+++ src/test/java/rest/CustomerResourceTest.java
@@ -1,20 +1,28 @@
 package rest;
 
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.ws.rs.core.Response;
 import dev.hv.db.CustomerDao;
 import dev.hv.db.DatabaseCon;
 import dev.hv.enums.Gender;
 import dev.hv.model.Customer;
 import dev.hv.rest.resource.CustomerResource;
+import org.everit.json.schema.Schema;
+import org.everit.json.schema.loader.SchemaLoader;
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.Objects;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
@@ -34,7 +42,7 @@ public class CustomerResourceTest {
     }
 
     @Test
-    public void testGetCustomerByUuid() throws SQLException {
+    public void testGetCustomerByUuid() throws SQLException, JsonProcessingException {
 
         // Arrange
         UUID testUuid = UUID.randomUUID();
@@ -44,12 +52,19 @@ public class CustomerResourceTest {
         when(mockCustomerDao.getCustomer(testUuid)).thenReturn(mockCustomer);
 
 
-        // Act
         Response response = customerResource.getCustomer(testUuid.toString());
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getEntity());
+        System.out.println(jsonResponse);
 
-        // Assert
         assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
         assertEquals(mockCustomer, response.getEntity());
+
+        JSONObject schemaJson = new JSONObject(new JSONTokener(Objects.requireNonNull(getClass().getResourceAsStream("/json schemas/JSON_Schema_Customer.json"))));
+        Schema schema = SchemaLoader.load(schemaJson);
+
+        JSONObject responseJson = new JSONObject(jsonResponse);
+        assertDoesNotThrow(() -> schema.validate(responseJson), "This JSON does not conform to the provided schema.");
     }
 
 }
