218c7d7021c722cd57b252d0875486fb0277fa2d
KilisAccount <vogel.kilian@online.de>
2025-01-23 11:49:42 +0100

    Added ReadingResource testGetByParameter Fails

--- src/test/java/rest/ReadingResourceTest.java
+++ src/test/java/rest/ReadingResourceTest.java
@@ -32,161 +32,204 @@ import dev.hv.rest.resource.ReadingResource;
 import jakarta.ws.rs.core.Response;
 
 public class ReadingResourceTest {
-    private static ReadingResource readingResource;
-    private static ReadingDao mockReadingDao;
-    private static DatabaseCon mockDbConnection;
-    private static Customer mockCustomer;
+        private static ReadingResource readingResource;
+        private static ReadingDao mockReadingDao;
+        private static DatabaseCon mockDbConnection;
+        private static Customer mockCustomer;
 
-    @BeforeAll
-    public static void before() throws SQLException {
-        mockDbConnection = mock(DatabaseCon.class);
-        when(mockDbConnection.getConnection()).thenReturn(mock(Connection.class));
+        @BeforeAll
+        public static void before() throws SQLException {
+                mockDbConnection = mock(DatabaseCon.class);
+                when(mockDbConnection.getConnection()).thenReturn(mock(Connection.class));
 
-        mockCustomer = new Customer(UUID.randomUUID(), "Hugh", "Jass", LocalDate.now(), Gender.D);
+                mockCustomer = new Customer(UUID.randomUUID(), "Hugh", "Jass", LocalDate.now(), Gender.D);
 
-        mockReadingDao = mock(ReadingDao.class);
-        readingResource = new ReadingResource(mockDbConnection, mockReadingDao);
-    }
+                mockReadingDao = mock(ReadingDao.class);
+                readingResource = new ReadingResource(mockDbConnection, mockReadingDao);
+        }
 
-    @Test
-    public void testDeleteReadingOk() throws SQLException, JsonProcessingException {
-        UUID mockReadingUuid = UUID.randomUUID();
-        Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
-                KindOfMeter.WASSER, 2.5, "12345", false);
+        @Test
+        public void testDeleteReadingOk() throws SQLException, JsonProcessingException {
+                UUID mockReadingUuid = UUID.randomUUID();
+                Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
+                                KindOfMeter.WASSER, 2.5, "12345", false);
 
-        Mockito.doNothing().when(mockReadingDao).deleteReading(mockReading);
-        when(mockReadingDao.getReading(mockReadingUuid)).thenReturn(mockReading);
+                Mockito.doNothing().when(mockReadingDao).deleteReading(mockReading);
+                when(mockReadingDao.getReading(mockReadingUuid)).thenReturn(mockReading);
 
-        Response response = readingResource.deleteReading(mockReadingUuid.toString());
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getEntity());
-        System.out.println(jsonResponse);
+                Response response = readingResource.deleteReading(mockReadingUuid.toString());
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter()
+                                .writeValueAsString(response.getEntity());
+                System.out.println(jsonResponse);
 
-        JSONObject schemaJson = new JSONObject(new JSONTokener(Objects.requireNonNull(
-                getClass().getResourceAsStream("/json schemas/JSON_Schema_Reading.json"))));
-        Schema schema = SchemaLoader.load(schemaJson);
+                JSONObject schemaJson = new JSONObject(new JSONTokener(Objects.requireNonNull(
+                                getClass().getResourceAsStream("/json schemas/JSON_Schema_Reading.json"))));
+                Schema schema = SchemaLoader.load(schemaJson);
 
-        JSONObject responseJson = new JSONObject(jsonResponse);
-        assertDoesNotThrow(() -> schema.validate(responseJson), "This JSON does not conform to the provided schema.");
-    }
+                JSONObject responseJson = new JSONObject(jsonResponse);
+                assertDoesNotThrow(() -> schema.validate(responseJson),
+                                "This JSON does not conform to the provided schema.");
+        }
 
-    @Test
-    public void testDeleteReadingNotFound() throws SQLException, JsonProcessingException {
-        UUID mockReadingUuid = UUID.randomUUID();
-        Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
-                KindOfMeter.WASSER, 2.5, "12345", false);
+        @Test
+        public void testDeleteReadingNotFound() throws SQLException, JsonProcessingException {
+                UUID mockReadingUuid = UUID.randomUUID();
+                Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
+                                KindOfMeter.WASSER, 2.5, "12345", false);
 
-        Mockito.doNothing().when(mockReadingDao).deleteReading(mockReading);
+                Mockito.doNothing().when(mockReadingDao).deleteReading(mockReading);
 
-        Response response = readingResource.deleteReading(mockReadingUuid.toString());
+                Response response = readingResource.deleteReading(mockReadingUuid.toString());
 
-        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
-        assertEquals("Could not find reading with uuid : " + mockReadingUuid, response.getEntity());
-    }
+                assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
+                assertEquals("Could not find reading with uuid : " + mockReadingUuid, response.getEntity());
+        }
 
-    @Test
-    public void testCreateReading() throws SQLException, JsonProcessingException {
-        UUID uuid = UUID.randomUUID();
-        Reading reading = new Reading(uuid, "test", mockCustomer, LocalDate.now(), KindOfMeter.HEIZUNG, 2.0, "1", true);
-        Mockito.doNothing().when(mockReadingDao).createReading(reading);
+        @Test
+        public void testCreateReading() throws SQLException, JsonProcessingException {
+                UUID uuid = UUID.randomUUID();
+                Reading reading = new Reading(uuid, "test", mockCustomer, LocalDate.now(), KindOfMeter.HEIZUNG, 2.0,
+                                "1", true);
+                Mockito.doNothing().when(mockReadingDao).createReading(reading);
 
-        Response response = readingResource.createReading(reading);
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getEntity());
-        System.out.println(jsonResponse);
+                Response response = readingResource.createReading(reading);
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter()
+                                .writeValueAsString(response.getEntity());
+                System.out.println(jsonResponse);
 
-        JSONObject schemaJson = new JSONObject(new JSONTokener(
-                Objects.requireNonNull(getClass().getResourceAsStream("/json schemas/JSON_Schema_Reading.json"))));
-        Schema schema = SchemaLoader.load(schemaJson);
+                JSONObject schemaJson = new JSONObject(new JSONTokener(
+                                Objects.requireNonNull(getClass()
+                                                .getResourceAsStream("/json schemas/JSON_Schema_Reading.json"))));
+                Schema schema = SchemaLoader.load(schemaJson);
 
-        assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
+                assertEquals(Response.Status.CREATED.getStatusCode(), response.getStatus());
 
-        JSONObject responseJson = new JSONObject(jsonResponse);
-        assertDoesNotThrow(() -> schema.validate(responseJson), "This JSON does not conform to the provided schema.");
-    }
+                JSONObject responseJson = new JSONObject(jsonResponse);
+                assertDoesNotThrow(() -> schema.validate(responseJson),
+                                "This JSON does not conform to the provided schema.");
+        }
 
-    @Test
-    public void testGetReading() throws SQLException, JsonProcessingException {
-        UUID uuid = UUID.randomUUID();
-        Reading reading = new Reading(uuid, "test", mockCustomer, LocalDate.now(), KindOfMeter.HEIZUNG, 2.0, "1", true);
-        Mockito.doReturn(reading).when(mockReadingDao).getReading(uuid);
+        @Test
+        public void testGetReading() throws SQLException, JsonProcessingException {
+                UUID uuid = UUID.randomUUID();
+                Reading reading = new Reading(uuid, "test", mockCustomer, LocalDate.now(), KindOfMeter.HEIZUNG, 2.0,
+                                "1", true);
+                Mockito.doReturn(reading).when(mockReadingDao).getReading(uuid);
 
-        Response response = readingResource.getReading(uuid.toString());
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getEntity());
-        System.out.println(jsonResponse);
+                Response response = readingResource.getReading(uuid.toString());
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter()
+                                .writeValueAsString(response.getEntity());
+                System.out.println(jsonResponse);
 
-        JSONObject schemaJson = new JSONObject(new JSONTokener(
-                Objects.requireNonNull(getClass().getResourceAsStream("/json schemas/JSON_Schema_Reading.json"))));
-        Schema schema = SchemaLoader.load(schemaJson);
+                JSONObject schemaJson = new JSONObject(new JSONTokener(
+                                Objects.requireNonNull(getClass()
+                                                .getResourceAsStream("/json schemas/JSON_Schema_Reading.json"))));
+                Schema schema = SchemaLoader.load(schemaJson);
 
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+                assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 
-        JSONObject responseJson = new JSONObject(jsonResponse);
-        assertDoesNotThrow(() -> schema.validate(responseJson), "This JSON does not conform to the provided schema.");
-    }
+                JSONObject responseJson = new JSONObject(jsonResponse);
+                assertDoesNotThrow(() -> schema.validate(responseJson),
+                                "This JSON does not conform to the provided schema.");
+        }
 
-    @Test
-    public void testUpdateReadingOk() throws SQLException {
-        UUID mockReadingUuid = UUID.randomUUID();
-        Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
-                KindOfMeter.WASSER, 2.5, "12345", false);
+        @Test
+        public void testUpdateReadingOk() throws SQLException {
+                UUID mockReadingUuid = UUID.randomUUID();
+                Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
+                                KindOfMeter.WASSER, 2.5, "12345", false);
 
-        Mockito.doNothing().when(mockReadingDao).updateReading(mockReading);
+                Mockito.doNothing().when(mockReadingDao).updateReading(mockReading);
 
-        Response response = readingResource.updateReading(mockReading);
+                Response response = readingResource.updateReading(mockReading);
 
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
-        assertEquals("Reading with uuid: " + mockReadingUuid + " was updated.", response.getEntity());
-    }
+                assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+                assertEquals("Reading with uuid: " + mockReadingUuid + " was updated.", response.getEntity());
+        }
 
-    @Test
-    public void testUpdateReadingNotFound() throws SQLException, JsonProcessingException {
-        UUID mockReadingUuid = UUID.randomUUID();
-        Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
-                KindOfMeter.WASSER, 2.5, "12345", false);
+        @Test
+        public void testUpdateReadingNotFound() throws SQLException, JsonProcessingException {
+                UUID mockReadingUuid = UUID.randomUUID();
+                Reading mockReading = new Reading(mockReadingUuid, "This is a comment", mockCustomer, LocalDate.now(),
+                                KindOfMeter.WASSER, 2.5, "12345", false);
 
-        Mockito.doThrow(new SQLException()).when(mockReadingDao).updateReading(mockReading);
+                Mockito.doThrow(new SQLException()).when(mockReadingDao).updateReading(mockReading);
 
-        Response response = readingResource.updateReading(mockReading);
+                Response response = readingResource.updateReading(mockReading);
 
-        assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
-        assertEquals("Could not find reading with uuid : " + mockReadingUuid, response.getEntity());
-    }
+                assertEquals(Response.Status.NOT_FOUND.getStatusCode(), response.getStatus());
+                assertEquals("Could not find reading with uuid : " + mockReadingUuid, response.getEntity());
+        }
 
-    @Test
-    public void testUpdateReadingNoUuid() throws SQLException, JsonProcessingException {
-        Reading mockReading = new Reading(null, "This is a comment", mockCustomer, LocalDate.now(),
-                KindOfMeter.WASSER, 2.5, "12345", false);
-        Mockito.doThrow(new SQLException()).when(mockReadingDao).updateReading(mockReading);
+        @Test
+        public void testUpdateReadingNoUuid() throws SQLException, JsonProcessingException {
+                Reading mockReading = new Reading(null, "This is a comment", mockCustomer, LocalDate.now(),
+                                KindOfMeter.WASSER, 2.5, "12345", false);
+                Mockito.doThrow(new SQLException()).when(mockReadingDao).updateReading(mockReading);
 
-        Response response = readingResource.updateReading(mockReading);
+                Response response = readingResource.updateReading(mockReading);
 
-        assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
-        assertEquals("Please provide a valid uuid!", response.getEntity());
-    }
+                assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+                assertEquals("Please provide a valid uuid!", response.getEntity());
+        }
 
-    @Test
-    public void testGetReadingsByParameter() throws SQLException, JsonProcessingException {
-        Reading mockReading = new Reading(UUID.randomUUID(), "This is a comment", mockCustomer, LocalDate.now(),
-                KindOfMeter.WASSER, 2.5, "12345", false);
-        when(mockReadingDao.getReadings(mockCustomer.getId(), LocalDate.of(2025, 1, 1), LocalDate.of(3025, 12, 12),
-                KindOfMeter.WASSER)).thenReturn(List.of(mockReading));
+        @Test
+        public void testGetReadingsByParameter() throws SQLException, JsonProcessingException {
+                Reading mockReading = new Reading(UUID.randomUUID(), "This is a comment", mockCustomer, LocalDate.now(),
+                                KindOfMeter.WASSER, 2.5, "12345", false);
+                when(mockReadingDao.getReadings(mockCustomer.getId(), LocalDate.of(2025, 1, 1),
+                                LocalDate.of(3025, 12, 12),
+                                KindOfMeter.WASSER)).thenReturn(List.of(mockReading));
 
-        Response response = readingResource.getReadingsByParameter(mockCustomer.getId().toString(), "2025-01-01",
-                "3025-12-12", "wAsSeR");
+                Response response = readingResource.getReadingsByParameter(mockCustomer.getId().toString(),
+                                "2025-01-01",
+                                "3025-12-12", "wAsSeR");
 
-        ObjectMapper objectMapper = new ObjectMapper();
-        String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(response.getEntity());
+                ObjectMapper objectMapper = new ObjectMapper();
+                String jsonResponse = objectMapper.writerWithDefaultPrettyPrinter()
+                                .writeValueAsString(response.getEntity());
 
-        JSONObject schemaJson = new JSONObject(new JSONTokener(
-                Objects.requireNonNull(getClass().getResourceAsStream("/json schemas/JSON_Schema_Readings.json"))));
-        Schema schema = SchemaLoader.load(schemaJson);
+                JSONObject schemaJson = new JSONObject(new JSONTokener(
+                                Objects.requireNonNull(getClass()
+                                                .getResourceAsStream("/json schemas/JSON_Schema_Readings.json"))));
+                Schema schema = SchemaLoader.load(schemaJson);
 
-        assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
+                assertEquals(Response.Status.OK.getStatusCode(), response.getStatus());
 
-        System.out.println(jsonResponse);
-        JSONObject responseJson = new JSONObject(jsonResponse);
-        assertDoesNotThrow(() -> schema.validate(responseJson), "This JSON does not conform to the provided schema.");
-    }
+                System.out.println(jsonResponse);
+                JSONObject responseJson = new JSONObject(jsonResponse);
+                assertDoesNotThrow(() -> schema.validate(responseJson),
+                                "This JSON does not conform to the provided schema.");
+        }
+
+        @Test
+        public void testGetReadingsByParameterNoCustomerId() throws SQLException, JsonProcessingException {
+                Response response = readingResource.getReadingsByParameter(null, null, null, null);
+
+                assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+                assertEquals("customerId is not defined", response.getEntity());
+        }
+
+        @Test
+        public void testGetReadingsByParameterWrongStartDateFOrmat() throws SQLException, JsonProcessingException {
+                UUID randomUuid = UUID.randomUUID();
+                Response response = readingResource.getReadingsByParameter(randomUuid.toString(), "2025.01.01", null,
+                                null);
+
+                assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+                assertEquals("Invalid start format", response.getEntity());
+        }
+
+        @Test
+        public void testGetReadingsByParameterWrongEndDateFOrmat() throws SQLException, JsonProcessingException {
+                UUID randomUuid = UUID.randomUUID();
+                Response response = readingResource.getReadingsByParameter(randomUuid.toString(), null, "2025.01.01",
+                                null);
+
+                assertEquals(Response.Status.BAD_REQUEST.getStatusCode(), response.getStatus());
+                assertEquals("Invalid end format", response.getEntity());
+        }
 }
